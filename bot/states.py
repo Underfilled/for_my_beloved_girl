e518af01f390fd744e36c396a0f7c03eae08bd19
Underfilled <underfilled@mail.ru>
2026-05-04 21:22:52 +0500

    Delete bot directory

--- bot/states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class UserState(StatesGroup):
-    choosing_mode = State()
-    roulette = State()
-    dialog_searching = State()
-    dialog_chatting = State()
